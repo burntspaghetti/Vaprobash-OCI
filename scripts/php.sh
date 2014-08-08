571f41ae787e5eb08f895e6b5111619724ead3f8
Joey Stowe <jbstowe@ua.edu>
2014-08-08 16:40:52 -0500

    remmoving php-fpm

--- scripts/php.sh
+++ scripts/php.sh
@@ -69,12 +69,16 @@ xdebug.var_display_max_data = 1024
 EOF
 
     # PHP Error Reporting Config
-    sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/fpm/php.ini
-    sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/fpm/php.ini
+    #sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/fpm/php.ini
+    #sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/fpm/php.ini
+    sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/php.ini
+    sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
 
     # PHP Date Timezone
-    sudo sed -i "s/;date.timezone =.*/date.timezone = ${2/\//\\/}/" /etc/php5/fpm/php.ini
+    #sudo sed -i "s/;date.timezone =.*/date.timezone = ${2/\//\\/}/" /etc/php5/fpm/php.ini
+    sudo sed -i "s/;date.timezone =.*/date.timezone = ${2/\//\\/}/" /etc/php5/apache2/php.ini
     sudo sed -i "s/;date.timezone =.*/date.timezone = ${2/\//\\/}/" /etc/php5/cli/php.ini
 
-    sudo service php5-fpm restart
+    #sudo service php5-fpm restart
+    sudo service apache2 restart
 fi
